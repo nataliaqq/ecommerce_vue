3357dc276cb0b6079d5e4faa6932b51c2a57c867
natalia <natalia@mikamai.com>
2020-10-27 23:27:49 +0100

    import svg wishlist icon in header

--- js-challange/src/components/Header.vue
+++ js-challange/src/components/Header.vue
@@ -10,10 +10,7 @@
                 <span class="bag__item-counter">10</span>
             </div>
             <div class="header-bag__item header-bag__wishlist-count">
-                <svg class="icon" width="20px" height="20px" viewBox="0 6 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <title>Wishlist Icon</title>
-                    <polygon id="Wishlist-Icon" stroke="none" fill-rule="evenodd" points="12.3598869 13.2675869 20 13.2675869 13.8200565 17.7545318 16.1782804 25.0221187 9.99833694 20.5318477 3.81839348 25.0221187 6.17994346 17.7545318 0 13.2675869 7.63678696 13.2675869 9.99833694 6"></polygon>
-                </svg>
+                <img :src="wishlistIcon">
                 <span class="bag__item-counter">5</span>
             </div>
         </aside>
@@ -23,12 +20,15 @@
 <script>
 
 import Bag from '../assets/svg/bag.svg';
+import Wishlist from '../assets/svg/wishlist.svg';
 
 export default {
   name: 'Header',
   data () {
     return {
         bagIcon: Bag
+        bagIcon: Bag,
+        wishlistIcon: Wishlist
     }
   }
 }
