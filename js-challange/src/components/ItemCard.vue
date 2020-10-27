6caf37124cd04ec9e26ee89a518698c06ea97a38
natalia <natalia@mikamai.com>
2020-10-27 23:32:33 +0100

    import svg wishlist to item card

--- js-challange/src/components/ItemCard.vue
+++ js-challange/src/components/ItemCard.vue
@@ -4,10 +4,7 @@
           <figure class="product__image-wrapper">
               <img class="product__image" :src="`${item.cover_image_url}?q=60&fit=crop&w=300`" :alt="item.title" itemprop="image"/>
               <button class="product__wishlist-button button button--round button--wishlist">
-                  <svg class="icon" width="20px" height="20px" viewBox="0 6 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                      <title>Wishlist Icon</title>
-                      <polygon id="Wishlist-Icon" stroke="none" fill-rule="evenodd" points="12.3598869 13.2675869 20 13.2675869 13.8200565 17.7545318 16.1782804 25.0221187 9.99833694 20.5318477 3.81839348 25.0221187 6.17994346 17.7545318 0 13.2675869 7.63678696 13.2675869 9.99833694 6"></polygon>
-                  </svg>
+                <img :src="wishlistIcon">
               </button>
           </figure>
           <div class="product__details">
@@ -24,8 +21,15 @@
 </template>
 
 <script>
+import Wishlist from '../assets/svg/wishlist.svg';
+
 export default {
   name: 'ItemCard',
+  data () {
+    return {
+      wishlistIcon: Wishlist
+    }
+  },
   // @TODO props validation
   props: {
     item: Object
