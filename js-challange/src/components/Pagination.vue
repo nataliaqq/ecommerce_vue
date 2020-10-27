25423b81d0da4be90e6cc8a9e74e37c0ea4f9679
natalia <natalia@mikamai.com>
2020-10-27 23:08:39 +0100

    dynamic pages

--- js-challange/src/components/Pagination.vue
+++ js-challange/src/components/Pagination.vue
@@ -2,17 +2,17 @@
     <nav class="pagination">
         <ul class="pagination__list">
             <li class="pagination__item">
-                <a href="#" class="pagination__link">
+                <a href="#" class="pagination__link" @click="prevPage">
                     <img :src="arrowLeft">
                 </a>
             </li>
             <li class="pagination__item" v-for="page in 10" :key="page">
-                <a href="#" class="pagination__link">
+                <a href="#" class="pagination__link" @click="goToPage(page)">
                     {{ page }}
                 </a>
             </li>
             <li class="pagination__item">
-                <a href="#" class="pagination__link">
+                <a href="#" class="pagination__link" @click="nextPage">
                     <img :src="arrowRight">
                 </a>
             </li>
@@ -29,11 +29,24 @@ export default {
   data () {
       return {
           arrowLeft: ArrowLeft,
-          arrowRight: ArrowRight
+          arrowRight: ArrowRight,
+          currentPage: 1,
+          maxPage: 10,
+          minPage: 1,
       }
   },
   methods: {
-    
+      nextPage () {
+          if (this.currentPage >= this.maxPage) return
+          this.currentPage++
+      },
+      prevPage () {
+          if (this.currentPage <= this.minPage) return
+          this.currentPage--
+      },
+      goToPage (newPage) {
+          this.currentPage = newPage
+      }
   },
 }
 </script>
