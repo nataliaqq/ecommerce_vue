d4b0033b77a4ce79a7f8811f9f57fcc9076c0ea9
natalia <natalia@mikamai.com>
2020-10-30 20:05:22 +0100

    test item card
    test item card

--- js-challange/src/tests/unit/expectedData.js
+++ js-challange/src/tests/unit/expectedData.js
@@ -1,16 +1,18 @@
+export const expectedItem = {
+  cover_image_url: "https://images.musement.com/cover/0002/50/la-creazione-di-adamo-vaticani_header-149100.jpeg",
+  title: "Biglietti salta fila per i Musei Vaticani con ingresso opzionale per la Basilica di San Pietro",
+  uuid: "8cf63a09-d148-4cfe-a427-ccd538b35185",
+  retail_price: {
+      currency: "EUR",
+      value: 24,
+      formatted_value: "€ 24.00",
+      formatted_iso_value: "24,00 €"
+  },
+  discount: 0,
+  description: "Prenota un ingresso prioritario esclusivo con assistenza, accedi ai Musei nel modo più rapido possibile e scegli tra audioguida e visita guidata!"
+}
+
 export const expectedData = [
-    {
-      cover_image_url: "https://images.musement.com/cover/0002/50/la-creazione-di-adamo-vaticani_header-149100.jpeg",
-      title: "Biglietti salta fila per i Musei Vaticani con ingresso opzionale per la Basilica di San Pietro",
-      uuid: "8cf63a09-d148-4cfe-a427-ccd538b35185",
-      retail_price: {
-          currency: "EUR",
-          value: 24,
-          formatted_value: "€ 24.00",
-          formatted_iso_value: "24,00 €"
-      },
-      discount: 0,
-      description: "Prenota un ingresso prioritario esclusivo con assistenza, accedi ai Musei nel modo più rapido possibile e scegli tra audioguida e visita guidata!"
-    }
+  expectedItem
 ]
 
---
+++ js-challange/src/tests/unit/itemCard.spec.js
@@ -0,0 +1,74 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import ItemCard from '../../components/ItemCard'
+import Vuex from 'vuex'
+import { expectedItem } from './expectedData'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('ItemCard', () => {
+  let itemCard
+  let props
+  let store
+  let mutations
+
+  beforeEach(() => {
+      props = {
+        item: expectedItem,
+      }
+
+      mutations = {
+        addToCart: jest.fn(),
+        removeFromCart: jest.fn(),
+        addToWishlist: jest.fn(),
+        removeFromWishlist: jest.fn(),
+      }
+
+      store = new Vuex.Store({
+        mutations
+      })
+
+      itemCard = shallowMount(ItemCard, {
+        propsData: props,
+        computed: {
+          itemsInCart: () => [],
+          itemsInWishlist: () => []
+        },
+        store,
+        localVue
+      })
+  });
+
+  it('product renders correctly', () => {
+    expect(itemCard.find('.product__title').text()).toEqual(itemCard.props().item.title)
+  })
+
+  it('adds item to cart by button click', () => {
+    itemCard.find('.product__add-to-cart').trigger('click')
+    expect(mutations.addToCart).toBeCalled()
+  })
+
+  it('removes item from cart by button click', () => {
+    itemCard.vm.itemsInCart = [expectedItem]
+
+    itemCard.vm.$nextTick().then(() => {
+      itemCard.find('.product__add-to-cart').trigger('click')
+      expect(mutations.removeFromCart).toBeCalled()
+    });
+  })
+
+  it('adds item to wishlist by button click', () => {
+    itemCard.find('.product__wishlist-button').trigger('click')
+    expect(mutations.addToWishlist).toBeCalled()
+  })
+
+  it('removes item from cart by button click', () => {
+    itemCard.vm.itemsInWishlist = [expectedItem]
+
+    itemCard.vm.$nextTick().then(() => {
+      itemCard.find('.product__wishlist-button').trigger('click')
+      expect(mutations.removeFromWishlist).toBeCalled()
+    });
+  })
+});
+  
